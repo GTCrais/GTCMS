8882aa8485b594b88ed554d7f88789f6646d25f2
GTCrais <tomislav@gtcrais.net>
2017-04-09 16:33:12 +0200

    Add default 404 page.

---
+++ src/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('front.templates.default')
+
+@section('content')
+
+	<style>
+		@import url(https://fonts.googleapis.com/css?family=Lato:100);
+		.gtcms {
+			position: absolute;
+			font-family: Lato, sans-serif;
+			font-weight: 100;
+			font-size: 100px;
+			text-align: center;
+			left: 0;
+			right: 0;
+			top: calc(50% - 50px);
+		}
+	</style>
+
+	<div class="gtcms">404 :(</div>
+
+@stop
