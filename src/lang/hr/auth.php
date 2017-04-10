8e2540139ca1b7e1c419cf964cdd4a5aaedcf9c9
GTCrais <tomislav@gtcrais.net>
2017-04-10 10:33:51 +0200

    Add missing language strings.

--- src/lang/hr/auth.php
+++ src/lang/hr/auth.php
@@ -2,6 +2,18 @@
 
 return [
 
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used during authentication for various
+	| messages that we need to display to the user. You are free to modify
+	| these language lines according to your application's requirements.
+	|
+	*/
+
+	'failed' => 'Netočni podaci za prijavu.',
 	'throttle' => 'Previše neuspjelih pokušaja. Pokušajte ponovo za :seconds sekundi.',
 	'attemptsLeft' => '{1} Imate još :attemptsLeft pokušaj.|[2,Inf] Imate još :attemptsLeft pokušaja.',
 
